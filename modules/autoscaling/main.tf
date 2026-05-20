c52fb878f83716b52a64cda49728af35a1ec5194
Sanchay0306 <123968160+Sanchay0306@users.noreply.github.com>
2026-05-20 08:52:25 +0530

    Update AMI filter to use Ubuntu 22.04
    Updated the AMI filter to use Ubuntu 22.04 images and added additional filters for virtualization type and state.

--- modules/autoscaling/main.tf
+++ modules/autoscaling/main.tf
@@ -14,11 +14,23 @@ data "cloudinit_config" "config" {
 
 data "aws_ami" "ubuntu" {
   most_recent = true
+
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-22.04-amd64-server-*"]
   }
-  owners = ["099720109477"]
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+
+  owners = ["099720109477"] # Canonical's official AWS account ID
 }
 
 resource "aws_launch_template" "webserver" {
